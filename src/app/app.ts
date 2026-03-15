6f51111e8fc21824320906e0d2d42ac2f54d66bd
JohanStragus <joanmartinarnaiz@gmail.com>
2026-03-15 20:13:31 +0100

    Métodos nuevos: addImage + saveImages + restoreImages

--- src/app/app.ts
+++ src/app/app.ts
@@ -15,24 +15,32 @@ export class App {
   protected readonly title = signal('random_images');
   public logged = signal(false); // saber si esta logeado o no.
   public username = signal(''); // nombre del usuario
+  public newImage = signal(''); // Guardar lo que el usuario escriba en el input
+
   private readonly IMG_PATH: string = 'img/'; // ruta de las imagenes
   private imgIndex = signal(0); // en que posición del array está la imagen
 
 
   // ATRIBUTO ARRAY
-  public imgFiles = signal<string[]>([
-    'goku.webp',
-    'vegeta.webp',
-    'piccolo.webp'
-  ]);
+  public imgFiles = signal<string[]>([]);
+
+  // CONSTRUCTOR (cuando se carga la aplicación, recuperamos las imágenes guardadas en localStorage)
+  constructor() {
+    this.restoreImages();
+  }
 
   // GETTERS
   get selectedImage(): string {
+    // Si no hay imágenes en el array, devuelve una cadena vacía.
+    if(this.imgFiles().length === 0) return '';
+    // Si hay imágenes, devuelve la ruta completa de la imagen seleccionada.
     return this.IMG_PATH + this.imgFiles()[this.imgIndex()];
   }
 
   // MÉTODOS
   public changeRandomImage(): void {
+    // Evitar que elija una imagen aleatoria cuando no hay.
+    if(this.imgFiles().length === 0) return;
     this.imgIndex.set(Math.floor(Math.random() * this.imgFiles().length));
   }
 
@@ -42,7 +50,52 @@ export class App {
 
   // 3. Método para iniciar sesión, 
   public login(): void {
+    // Evitar que se inicie sesión si no hay nada introducido.
     if (this.username().trim() === '') return;
     this.logged.set(true);
+  }
+
+  // 4. Agregar una nueva imagen al array
+  public addImage(): void{
+    const newImageName = this.newImage().trim(); // suelo poner Trim para evitar espacios al principio o al final.
+
+    // Evitar agregar imagene si el nombre está vacio o si ya existe en el array.
+    if(newImageName === '' || this.imgFiles().includes(newImageName)) return;
+
+    // Agregar la nueva imagen al array.
+    this.imgFiles.update(current => [...current, newImageName]);
+
+    // Seleccionamos la nueva imagen, que será la última del array.
+    this.imgIndex.set(this.imgFiles().length - 1);
+
+    // Limpiar el input después de agregar la imagen.
+    this.newImage.set('');
+
+    // guardar en localStorage (llamar a la función más tarde)
+    this.saveImages();
+  }
+
+  // 5. Método para guardar en localStorage
+  public saveImages(): void{
+    // Guardar en localStorage las imagenes en un formato String
+    localStorage.setItem('IMG_FILES', JSON.stringify(this.imgFiles()));
+  }
+
+  // 6. Recuperar las imágenes de localStorage.
+  public restoreImages(): boolean{
+    // variables
+    const savedImages = localStorage.getItem('IMG_FILES');
+
+    // Si no hay nada vacío
+    if(savedImages != null){
+      // las recuperamos y las convertimos de nuevo a un array.
+      this.imgFiles.set(JSON.parse(savedImages));
+      // Evitamos acceder a una posición que no existe cuando cargarmos localStorage
+      this.imgIndex.set(0);
+      // devolvemos que se ha podido encontrar las imágenes.
+      return true;
+    }
+    // si no entra en el if significa que está vacío, por lo que devolvemos false.
+    return false;
   }
 }
